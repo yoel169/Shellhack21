745adf6ca934f8d5430cff978a94849744a8e49a
yoel169 <yoel169@gmail.com>
2021-09-25 17:17:59 -0500

    added test cases and functionality to store info

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import logging
 import traceback
 import json
+from datetime import datetime
 
 debug=True
 
@@ -27,61 +28,104 @@ def storeQueryFunction(intent_request):
     session_attributes = get_session_attributes(intent_request)
 
     #get slot values
-    city_name = get_slot(intent_request,'City')
-    hours_type =  get_slot(intent_request,'HoursType')
+    #city_name = get_slot(intent_request,'City')
+    department =  get_slot(intent_request,'Department')
     day_of_week = get_slot(intent_request,'DayOfWeek')
 
-    reply = ""
+    #reformat input strings
+    day_of_week = day_of_week.upper()
+    department = department.lower()
 
-    #check if city is correct
-    if city_name == storeInfo['address']['city']:
-        hour_query = ""
+    #check for store hours based on client input
+    storeHours = checkStoreInfo(day_of_week, department, storeInfo)
 
-        #reformat input strings
-        day_of_week = day_of_week.upper()
-        hours_type = hours_type.lower()
-
-        department, hour_query = checkDepartment(hours_type)
-
-        #check for day of week
-        for table in storeInfo[hour_query]:
-            if day_of_week == table["day"]:
-                opening = table["openTime"]
-                closing = table["closeTime"]
-
-        reply = """Our {} hours are from {} to {}.
-                            """.format(department, opening, closing)
-
-    else:
-
-        reply = """I'm sorry. We currently have no stores in {}.  
-        We are currently located at {}.""".format(city_name, storeInfo['address']['city'])
-
-    #reply message
+    #insert store hours
     message =  {
                 'contentType': 'PlainText',
-                'content': reply
+                'content': storeHours
                 }
     
     #fulffiled
     fulfillment_state = "Fulfilled"
 
+    #return reply
     return close(intent_request, session_attributes, fulfillment_state, message)
 
 
-#check for which department
-def checkDepartment(hours_type):
-    if "sales" in hours_type:
-        department = "Sales"
-        hour_query = "salesHours"
-    elif "service" in hours_type:
-        department = "Service"
-        hour_query = "serviceHours"
-    elif "collision" in hours_type:
-        department = "Collision Center"
-        hour_query = "collisionHours"
+#check for store hours
+def checkStoreInfo(day, dep, storeInfo):
 
-    return department, hour_query
+    #check which department
+    if "sales" in dep:
+        department = "Sales"
+        dep_query = "salesHours"
+    elif "service" in dep:
+        department = "Service"
+        dep_query = "serviceHours"
+    elif "collision" in dep:
+        department = "Collision Center"
+        dep_query = "collisionHours"
+    else:
+        return "Could not find that department. Our departments are: Sales, Service, and Collision."
+
+    #check if user actually checked for a possible day
+    possibleDays = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','WEEKDAY','WEEKEND'] 
+    if not day in possibleDays:
+        return "Can not find hours for {}.".format(day)
+
+    #check if user is asking for weekday
+    if day == "WEEKDAY":
+        hoursString = ""
+        for table in storeInfo[dep_query]:
+            opening = table["openTime"]
+            closing = table["closeTime"]
+
+            hoursString += "{}: from {} to {}.\n\n".format(table["day"],reformatTime(opening), reformatTime(closing))
+
+        return "Our {} is open on: \n {}".format(department, hoursString)
+
+    #check if user asked for weekend hours
+    elif day == "WEEKEND":
+        
+        salBool = False
+        sunBool = False
+
+
+        for table in storeInfo[dep_query][5:]:
+            
+            #check if department is open saturday
+            if table["day"] == "SATURDAY":
+              
+                satOpen = reformatTime(table["openTime"])
+                satClose = reformatTime(table["closeTime"])
+                salBool = True
+
+             #check if department is open sunday
+            if  table["day"] == "SUNDAY":
+              
+                sunOpen = reformatTime(table["openTime"])
+                sunClose = reformatTime(table["closeTime"])
+                sunBool = True
+        
+        if salBool and sunBool:
+            return "Our {} is open on Saturday from {} to {} and on Sunday from {} to {}.".format(department, satOpen, satClose, sunOpen, sunClose)
+        elif salBool:
+            return "Our {} is open on Saturday from {} to {} and closed on Sunday.".format(department, satOpen, satClose)
+        else:
+            return "Our {} is closed on Saturday and Sunday.".format(department)
+    
+    #check for a specific day
+    for table in storeInfo[dep_query]:
+        if day == table["day"]:
+            opening = table["openTime"]
+            closing = table["closeTime"]
+
+            return "Our {} hours are from {} to {}.".format(department, reformatTime(opening), reformatTime(closing))
+
+    return "Our {} is closed on {}.".format(department, (day[:1] + day[1:].lower()))
+
+def reformatTime(time):
+    return datetime.strptime(time, "%H:%M").strftime("%I:%M %p")
 
 #------------------------ Intents Dispatcher----------------------------------#
 def dispatch(intent_request):
